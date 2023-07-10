67af9f28b2770a524d9be3c973bbab065631a3cb
romeo-sarkar <romeosarkar2002@gmail.com>
2023-07-11 03:08:22 +0530

    upgraded to a new level

---
+++ object.js
@@ -0,0 +1,9 @@
+let object =
+{
+    a: 10,
+    b: 20,
+    c: this.a + this.b,
+};
+
+
+console.log(object);
--- script.js
+++ script.js
@@ -1,147 +1,209 @@
-'use strict';
-/*
-let btn = document.querySelector('.message')
-let btn_text = btn.textContent;
-
-btn_text = 'hello world';
-
-document.querySelector('.message').textContent = '🥳 correct number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 11000;
-
-document.querySelector('.guess').value = 100;
-*/
-
-// let doc;
-let highScore;
-let secretNumber, score;
-let arrGuesses;
-let gameOver;
-
-function gameInitialize()
+"use strict";
+const game =
 {
-    highScore = 0;
-}
+    min: 1,
+    max: 10,
 
-function gameStart()
-{
-    score = 10;
-    gameOver = false;
-    secretNumber = Math.trunc(Math.random() * 20) + 1;
-    arrGuesses =
-    [
-        false, false, false, false, false,
-        false, false, false, false, false,
-        false, false, false, false, false,
-        false, false, false, false, false
-    ];
-
-    // console.log(secretNumber);
-
-    document.querySelector('.guess').textContent = '';
-    document.querySelector('.number').textContent = '?';
-    document.querySelector('.score').textContent = score;
-    document.querySelector('.number').style.width = '25rem';
-    document.querySelector('.highscore').textContent = highScore;
-    document.querySelector('body').style.backgroundColor = '#1e1a1a';
-    document.querySelector('.message').textContent = '😀 start guessing!';
-}
-
-function gameInput(guess)
-{
-    if(arrGuesses[guess - 1])
+    initialize()
     {
-        return;
-    }
+        document.querySelector(".between").textContent = `between ${this.min} and ${this.max} (inclusive)`;
+        this.scoreMax = 2 * Math.trunc(Math.log(this.max - this.min + 1));
+        this.highScore = 0;
+        
+        this.domInitialize();
+        this.start();
 
-    if(guess < secretNumber)
+    },
+    domInitialize()
     {
-        score--;
-        document.querySelector('.message').textContent = '📉 too low!';
-    }
-    else if(guess > secretNumber)
-    {
-        score--;
-        document.querySelector('.message').textContent = '📈 too high!';
-    }
-    else
-    {
-        gameWon();
-        return;
-    }
+        this.domInputGuess = document.querySelector(".guess");
+        this.domDisplayMessage = document.querySelector(".message");
+        this.domDisplayBody = document.body;
     
-    document.querySelector('.score').textContent = score;
+        this.domSectionLeft = document.querySelector(".section-left");
+
+        document.querySelector("#btn-reload").addEventListener("click", this.domBtnReloadEventOnClick.bind(this));
+        document.querySelector("#btn-check").addEventListener("click", this.domBtnCheckEventOnClick.bind(this));
+        document.querySelector("#btn-again").addEventListener("click", this.domBtnAgainEventOnClick.bind(this));
+        
+        document.querySelector(".guess").addEventListener("keypress", this.domInputGuessEventOnKeyPress.bind(this));
+        // new Audio("res\\init.mp3").play();
+    },
+
     
-    if(score === 0)
+
+    random() // generates a random number between [this.min, this.max]
     {
-        gameLost();
-        return;
-    }
+        let rand = Math.random() * (this.max - this.min + 1);
+        return Math.trunc(rand + this.min);
+    },
+
+    start()
+    {
+        this.isGameOver = false;
+        this.score = this.scoreMax;
+        this.setGuesses = new Set();
+        this.secretNumber = this.random();
+        
+        this.domConfigure();
+        console.log(`secretNumber: ${this.secretNumber}`);
+    },
     
-    arrGuesses[guess - 1] = true;
-}
-
-function gameWon()
-{
-    gameOver = true;
+    check(number)
+    {
+        if(number == this.secretNumber)
+        {
+            this.win();
+            return;
+        }
+        
+        if(this.setGuesses.has(number))
+        {
+            new Audio("res/check-has.mp3").play();
+        }
+        else
+        {
+            this.score--;
+            this.setGuesses.add(number);
+            this.domUpdateScore();
+            new Audio("res/check.mp3").play();
+        }
+        this.domUpdateMessage(number);
+        
+        if(0 == this.score)
+        {
+            this.lose();
+        }
+    },
     
-    if(score > highScore)
+    win()
     {
-        highScore = score;
-        document.querySelector('.highscore').textContent = highScore;
-    }
+        if(this.score > this.highScore)
+        {
+            this.highScore = this.score;
+            this.domUpdateHighScore();
+        }
+
+        this.domUpdateNumber();
+        this.domDisplayBody.classList.add("win");
+        this.domDisplayMessage.textContent = "🥳 correct number";
+        new Audio("res/win.mp3").play();
+        
+        this.gameOver();
+    },
     
-    document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.message').textContent = '🥳 correct number!';
-}
-
-function gameLost()
-{
-    gameOver = true;
-
-    document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('body').style.backgroundColor = '#ab2222';
-    document.querySelector('.message').textContent = 'you lost the game!'
-}
-
-
-
-function checkOnClick()
-{
-    if(gameOver)
+    lose()
     {
-        return;
-    }
-
-    let guess = document.querySelector('.guess').value;
-    guess = Number(guess);
-
-    if(guess <= 0 || guess > 20)
+        this.domUpdateNumber();
+        this.domDisplayBody.classList.add("lose");
+        this.domDisplayMessage.textContent = "😭 you lose";
+        new Audio("res/lose.mp3").play();
+        this.gameOver();
+    },
+    
+    gameOver()
     {
-        document.querySelector('.message').textContent = '😫 invalid input!';
-        return;
+        this.isGameOver = true;
+        this.domDisplayMessage.classList.add("message-game-over");
+        this.domSectionLeft.classList.add("hidden");
+    },
+    
+    restart()
+    {
+        this.start();
+        new Audio("res/again.mp3").play();
+    },
+    
+    domConfigure()
+    {
+        this.domUpdateScore();
+        this.domUpdateHighScore();
+        this.domDisplayMessage.textContent = "😀 start guessing";
+        this.domDisplayBody.classList.remove("win", "lose");
+
+        this.domSectionLeft.classList.remove("hidden");
+        document.querySelector(".number").textContent = "";
+        this.domDisplayMessage.classList.remove("message-game-over");
+        
+    },
+
+    domUpdateScore()
+    {
+        let domDisplayScore = document.querySelector(".score");
+        domDisplayScore.textContent = String(this.score);
+    },
+
+    domUpdateMessage(number)
+    {
+        if(number < this.min || number > this.max)
+        {
+            this.domUpdateMessageInvalidNumber();
+            return;
+        }
+        
+        if(number > this.secretNumber)
+        {
+            this.domDisplayMessage.textContent = "📈 too high";
+        }
+        else
+        {
+            this.domDisplayMessage.textContent = "📉 too low";
+        }
+    },
+
+    domUpdateMessageInvalidNumber()
+    {
+        this.domDisplayMessage.textContent = "😫 invalid number";
+    },
+
+    domUpdateHighScore()
+    {
+        let domDisplayHighScore = document.querySelector(".high-score");
+        domDisplayHighScore.textContent = String(this.highScore);
+    },
+
+    domUpdateNumber()
+    {
+        let domDisplayNumber = document.querySelector(".number");
+        domDisplayNumber.textContent = String(this.secretNumber);
+    },
+
+    domBtnCheckEventOnClick()
+    {
+        console.log("dom check event on click");
+        console.log(this);
+        if(this.isGameOver)
+        {
+            return;
+        }
+
+        let input = this.domInputGuess.value;
+        if(input == "")
+        {
+            this.domUpdateMessageInvalidNumber();
+            return;
+        }
+
+        this.check(Number(input));
+    },
+
+    domInputGuessEventOnKeyPress(event)
+    {
+        console.log("keypress");
+        if(event.key == "Enter")
+        {
+            this.domBtnCheckEventOnClick();
+        }
+    },
+
+    domBtnAgainEventOnClick()
+    {
+        this.restart();
+    },
+    domBtnReloadEventOnClick()
+    {
+        location.reload();
     }
-
-    gameInput(guess);
 }
 
-function againOnClick()
-{
-    gameStart();
-}
-
-
-
-
-gameInitialize();
-
-gameStart();
-
-document.querySelector('.check').addEventListener('click', checkOnClick);
-document.querySelector('.again').addEventListener('click', againOnClick);
-
-
+game.initialize();
